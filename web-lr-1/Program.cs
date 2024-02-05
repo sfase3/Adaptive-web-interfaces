4741a892f6d28c93ed489009aeb8025e46776fdc
I-Kovalov <kovalevvanya50@gmail.com>
2024-02-06 01:45:14 +0200

    lr2
    .NET Class Library

--- web-lr-1/Program.cs
+++ web-lr-1/Program.cs
@@ -1,104 +1,94 @@
-﻿namespace WEB_LR_1;
-using System;
-using System.IO;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Threading;
 
 class Program
 {
     static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        // 1 System.Collections
+        DoCollection();
 
-        do
-        {
-            DisplayMenu();
+        // 2 Робота з датами використовуючи System.DateTime
+        ManipulateDates();
 
-            Console.Write("Оберіть варіант меню: ");
-            if (int.TryParse(Console.ReadLine(), out int choice))
-            {
-                switch (choice)
-                {
-                    case 1:
-                        CountWords();
-                        break;
+        // 3 потоки з System.Threading
+        PerformMultithreading();
 
-                    case 2:
-                        DoMath();
-                        break;
+        // 4 діагностика з System.Diagnostics
+        ShowProcessInfo();
 
-                    case 0:
-                        Console.WriteLine("poka!");
-                        return;
-
-                    default:
-                        Console.WriteLine("Неправильний варіант");
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Неправильний варіант");
-            }
-
-            Console.WriteLine();
-        } while (true);
+        // 5 інтернет операції з System.Net
+         DownloadWebPage("https://uk.javascript.info/");
+        Console.ReadLine();
     }
 
-
-    static void DisplayMenu()
+    // System.Collections
+    static void DoCollection()
     {
-        Console.WriteLine("Меню:");
-        Console.WriteLine("0. Вихід");
-        Console.WriteLine("1. Підрахувати кількість слів");
-        Console.WriteLine("2. Випадковий математичний вираз");
+        ArrayList list = new ArrayList();
+        list.Add("Hello");
+        list.Add("world");
+        list.Add("its");
+        list.Add("time");
+        list.Add("to");
+        list.Add("play");
+        list.Add("with");
+        list.Add("me");
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
     }
 
-    // 1
-    static void CountWords()
+    // System.DateTime
+    static void ManipulateDates()
     {
-        Console.Write("Введіть текст: ");
-        string? userInput = Console.ReadLine();
-
-        MatchCollection wordMatches = Regex.Matches(userInput, "[a-zA-Z0-9]");
-        string[] wordsArray;
-
-        if (wordMatches.Count() > 0)
-        {
-            wordsArray = userInput.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine($"Кількість слів: {wordsArray.Length}");
-            return;
-        }
-
-        string currentDirectoryPath = Directory.GetCurrentDirectory();
-        string fileContent = File.ReadAllText(Path.Combine(currentDirectoryPath, "loremipsum.txt"));
-        wordsArray = fileContent.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-        Console.WriteLine($"Зміст файлу: {fileContent}");
-        Console.WriteLine($"Кількість слів: {wordsArray.Length}");
-
-
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine("Теперешня дата: " + currentDate);
+        DateTime futureDate = currentDate.AddDays(12);
+        Console.WriteLine("Дата через 12 днів: " + futureDate);
     }
 
-    // 2
-    static void DoMath()
+    // System.Threading
+    static void PerformMultithreading()
     {
-        Console.Clear();
+        Thread myThread = new Thread(MyThreadMethod);
+        myThread.Start();
 
-        Console.Write("Введіть математичний вираз: ");
-        string text = Console.ReadLine();
-
-        try
+        for (int i = 0; i < 5; i++)
         {
-            System.Data.DataTable table = new System.Data.DataTable();
-            double result = Convert.ToDouble(table.Compute(text, string.Empty));
-            Console.WriteLine($"Результат: {result}");
+            Console.WriteLine($"Основний потік: {i}");
+            Thread.Sleep(1000);
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Помилка: {ex.Message}");
-        }
+    }
 
-        Console.WriteLine("Натисніть будь-яку клавішу для продовження...");
-        Console.ReadKey();
+    static void MyThreadMethod()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Вторинний потік: {i}");
+            Thread.Sleep(1500);
+        }
+    }
+
+    // System.Diagnostics
+    static void ShowProcessInfo()
+    {
+        Process currentProcess = Process.GetCurrentProcess();
+        Console.WriteLine("Ідентифікатор процесу: " + currentProcess.Id);
+        Console.WriteLine("Назва процесу: " + currentProcess.ProcessName);
+    }
+
+    // System.Net
+    static void DownloadWebPage(string url)
+    {
+        using (System.Net.WebClient client = new System.Net.WebClient())
+        {
+            string content = client.DownloadString(url);
+            Console.WriteLine("Веб сторінка складає: " + content.Length + " символів");
+        }
     }
 }
